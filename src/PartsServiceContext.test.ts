2f4ceab32273d0f80d3111d43acc4282b1b426ad
Simon Pritchard <simon@conceptfusion.co.nz>
2021-07-29 12:21:27 +1200

    Additional unit test

--- src/PartsServiceContext.test.ts
+++ src/PartsServiceContext.test.ts
@@ -3,6 +3,7 @@ import { isValidPartNumber } from './PartsServiceContext';
 test.each([
   ['111-abcd', false],
   ['a111-abcd', false],
+  ['1111-abc', false],
   ['1111abcd', false],
   ['111 1a bcd', false],
   ['1111-123abcd', true],
